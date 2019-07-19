3795bf1b3f8731c9ad0a19d148c18de0f2ca1142
AJ1326 <abhishek.jaimini@docdoc.com>
2019-07-20 03:35:47 +0530

    minor change

--- src/app/shared/utils/form-helper.ts
+++ src/app/shared/utils/form-helper.ts
@@ -3,8 +3,11 @@ import { mapValues } from 'lodash';
 export class FormHelper {
   public static markAllAsDirty(formGroup: FormGroup | FormArray): void {
     mapValues(formGroup.controls, c => {
-      if (c instanceof FormGroup || c instanceof FormArray) FormHelper.markAllAsDirty(c);
-      else c.markAsDirty();
+      if (c instanceof FormGroup || c instanceof FormArray) {
+        FormHelper.markAllAsDirty(c);
+      } else {
+        c.markAsDirty();
+      }
     });
   }
 }
