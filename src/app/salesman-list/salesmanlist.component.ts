8af44fa654eb3978bbb634850d96082780905980
AJ1326 <jaiminiabhishek@gmail.com>
2019-10-08 18:40:34 +0530

    addition of salesman list modal.

--- src/app/salesman-list/salesmanlist.component.ts
+++ src/app/salesman-list/salesmanlist.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { environment } from '@env/environment';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-salesman-list',
@@ -9,8 +10,30 @@ import { environment } from '@env/environment';
 })
 export class SalesmanlistComponent implements OnInit {
   version: string = environment.version;
+  closeResult: string;
 
-  constructor() {}
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {}
+
+  public openModal(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(
+      result => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      reason => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      }
+    );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
